bdf6b9d30e09e2ab47b39b4ca5b2433f4f76f5b9
christian <christiangaribaldi@hotmail.com>
2016-09-27 12:50:54 -0300

    * Finalizaçao da tela de Venda - logica para add/remover carrinho e calcular valor total com desconto

--- app/assets/javascripts/vendas_carrinho.js
+++ app/assets/javascripts/vendas_carrinho.js
@@ -2,29 +2,73 @@
  * Created by christian on 26/09/16.
  */
 
-function adiciona_carrinho(evento){
-    debugger;
+function adiciona_produto_carrinho(evento){
     var botao = evento.target;
     var id_produto = $(botao).val();
-    //var seletor_linha = "#linha_estoque_" + id_produto;
-    //$(seletor_linha).prop("disabled",true);
+
+    //Desabilita botão de adicionar do produto clicado
     $(botao).prop("disabled",true);
 
-    //fazer cópia da linha
+    //Cópia da Linha de Estoque
+    var seletor_linha = "#linha_estoque_" + id_produto;
+    var copia = $(seletor_linha).clone();
 
+    var nome = copia.find(".nome").html();
+    var preco = copia.find(".preco").html();
+
+    var nova_linha =
+        "<tr id='linha_carrinho_"+ id_produto +"'>" +
+            "<td class='nome'>" + nome +" </td>" +
+            "<td class='preco'>" + preco +" </td>" +
+            "<td> <span class='col-md-10'> <input name='quantidade' value='1' type='number' class='form-control' min='1' step='1'/> </span> </td>" +
+            "<td><button id='bt_remove' value='"+ id_produto +"' class='btn btn-danger btn-sm'>-</button>" +
+            "</td>" +
+        "</tr>";
+
+    $(".lista_carrinho:last").append(nova_linha);
+
+    //Adiciono manipulador de evento ao novo botão de Remover do Carrinho
+    $("[id*='bt_remove']" ).on("click",function(event) {
+        event.preventDefault();
+        remove_produto_carrinho(event);
+        calcula_valor_carrinho();
+    });
+
+    //Adiciono manipulador de evento ao novo campo 'Quantidade'.
+    $("[name*='quantidade']" ).on("change",function(event) {
+        event.preventDefault();
+        calcula_valor_carrinho();
+    });
 }
 
-function remove_carrinho(evento){
-    debugger;
+function remove_produto_carrinho(evento){
     var botao = evento.target;
     var id_produto = $(botao).val();
+
+    //Habilita botão de adicionar do produto removido do carrinho
     var seletor_botao = "#bt_add_" + id_produto;
     $(seletor_botao).prop("disabled",false);
 
-    //remover linha da tabela
+    //Remove produto do carrinho
+    var seletor_linha = "#linha_carrinho_" + id_produto;
+    $(seletor_linha).remove();
 }
 
-
 function calcula_valor_carrinho(){
+    var valor_total = 0;
+    $(".lista_carrinho").children().each(function(index,elemento){
+        debugger;
+        var qtd = $(elemento).find("[name='quantidade']").val();
+        var preco = $(elemento).find(".preco").text();
+        if(qtd == "" || preco == "") valor_total+=0;
+        else valor_total+= qtd * preco;
+    });
 
+    // Calcula Valor Total com desconto
+    var desconto = $("#desconto").val();
+    if(desconto != "") valor_total *= (1 - (desconto/100));
+
+    // Exibe Valor Total
+    valor_total = valor_total.toFixed(2);
+    $("#valor_total").text(valor_total);
 }
